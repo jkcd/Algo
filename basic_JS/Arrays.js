320f32f6b0816d6e63d580bfdb2ea09735d09f9c
Jeyakrishna Ramamoorthy <jramamoorthy@paypal.com>
2017-01-29 16:05:50 +0530

    string manipulation

--- basic_JS/Arrays.js
+++ basic_JS/Arrays.js
@@ -1,3 +1,36 @@
 /**
- * Created by jramamoorthy on 28/01/17.
+ * Created by jkcd on 28/01/17.
+ *
+ * Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers
+ *
+ *
+ *
  */
+
+var rawInput = '';
+var inputLinesArray;
+var outputArray = [];
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+process.stdin.on('data', function(data){
+    rawInput += data;
+});
+
+process.stdin.on('end', function(){
+    inputLinesArray = rawInput.split('\n');
+    inputLinesArray = rawInput.split(' ');
+    inputLinesArray = inputLinesArray.map(Number);
+    main();
+})
+
+
+function main(){
+    console.log('input: ' + inputLinesArray);
+    for ( var i = inputLinesArray.length- 1, j= 0; i >= 0; i--, j++){
+        outputArray[j] = inputLinesArray[i];
+    }
+    console.log('output: ' + outputArray);
+
+}
--- basic_JS/StringProg.js
+++ basic_JS/StringProg.js
@@ -1,3 +1,49 @@
 /**
- * Created by jramamoorthy on 27/01/17.
+ * Created by jkcd on 27/01/17.
+ *
+ *
+ * Given a string S , of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as
+ *  2 space-separated strings on a single line (see the Sample below for more detail).
+ *
+
+ *
  */
+
+
+
+var rawInput = '';
+var inputLinesArray;
+var evenString = '';
+var oddString = '';
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+
+process.stdin.on('data', function(data){
+    rawInput += data;
+});
+
+process.stdin.on('end', function(){
+    inputLinesArray = rawInput.split('\n');
+    console.log(rawInput);
+    main();
+});
+
+function main(){
+    var inputString = inputLinesArray[0];
+    console.log(inputString);
+
+    for ( var i = 0; i < inputString.length; i++ ){
+        if ( i % 2 ){
+            oddString += inputString[i];
+        }
+        else {
+            evenString += inputString[i];
+        }
+
+    }
+    console.log('even string: ' + evenString);
+    console.log('odd string: ' + oddString );
+
+}
